0c1f691dcacefc79fd2e0e485d222c96cd3b988b
italomaia03 <italo.fernandes.om@gmail.com>
2023-03-26 21:04:06 -0300

    implement new courses model

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesComponent } from './courses/courses.component';
 import { CoursesRoutingModule } from './courser-routing.module';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [CoursesComponent],
-  imports: [CommonModule, CoursesRoutingModule],
+  imports: [CommonModule, CoursesRoutingModule, MatTableModule],
 })
 export class CoursesModule {}
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
-
+  courses: Course[] = [{ id: '1', name: 'Angular', category: 'Front-End' }];
+  displayedColumns = ['name', 'category'];
 }
